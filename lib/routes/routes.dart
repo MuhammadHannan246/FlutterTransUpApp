336097016dfa8ad657765030f8e7a6f9dfe9893d
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-08-31 16:03:24 +0500

    Update routes.dart

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter_transup_app/screens/forgot_password_screen.dart';
+import 'package:flutter_transup_app/screens/onboarding_screen.dart';
 import 'package:flutter_transup_app/screens/sign_in_screen.dart';
 import 'package:flutter_transup_app/screens/sign_up_screen.dart';
 import 'package:flutter_transup_app/screens/splash_screen.dart';
@@ -9,6 +10,7 @@ class PageRoutes {
   Map<String, WidgetBuilder> routes() {
     return {
       SplashScreen.routeName: (context) => const SplashScreen(),
+      OnboardingScreen.routeName: (context) => const OnboardingScreen(),
       SignInScreen.routeName: (context) => const SignInScreen(),
       SignUpScreen.routeName: (context) => const SignUpScreen(),
       ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
